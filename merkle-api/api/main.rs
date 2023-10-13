32155ce07015ae136ac8f03b91433a72dfe12d03
Mircea Gavriliu <gavriliumircea@yahoo.ro>
2023-10-13 18:50:55 +0300

    feat: add support for warp lambda

--- merkle-api/api/main.rs
+++ merkle-api/api/main.rs
@@ -1,3 +1,4 @@
+use dotenvy::dotenv;
 use futures::stream::{StreamExt, TryStreamExt};
 use warp::{multipart::FormData, Rejection};
 
@@ -11,7 +12,19 @@ type WebResult<T> = std::result::Result<T, Rejection>;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     let routes = controller::build_routes();
+
+    if let Ok(mode) = std::env::var("RUN_MODE") {
+        if mode == "LAMBDA" {
+            let warp_service = warp::service(routes);
+            warp_lambda::run(warp_service)
+                .await
+                .expect("An error occured");
+            return;
+        }
+    }
 
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
