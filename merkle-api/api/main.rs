32e5538540ba6417ae2e6e5b62da0b71585e5540
Mircea Gavriliu <gavriliumircea@yahoo.ro>
2023-10-16 15:25:14 +0300

    chore: test warp_lambda service without env vars

--- merkle-api/api/main.rs
+++ merkle-api/api/main.rs
@@ -1,6 +1,6 @@
 use dotenvy::dotenv;
 use futures::stream::{StreamExt, TryStreamExt};
-use warp::{multipart::FormData, Rejection, Filter};
+use warp::{multipart::FormData, Filter, Rejection};
 
 mod controller;
 mod csv_campaign_parser;
@@ -12,20 +12,25 @@ type WebResult<T> = std::result::Result<T, Rejection>;
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    // dotenv().ok();
 
+    // if let Ok(mode) = std::env::var("RUN_MODE") {
+    //     if mode == "LAMBDA" {
+    //         let routes = warp::any().map(|| "Hello, World!");
+    //         let warp_service = warp::service(routes);
+    //         warp_lambda::run(warp_service)
+    //             .await
+    //             .expect("An error occured");
+    //         return;
+    //     }
+    // }
 
-    if let Ok(mode) = std::env::var("RUN_MODE") {
-        if mode == "LAMBDA" {
-            let routes = warp::any().map(|| "Hello, World!");
-            let warp_service = warp::service(routes);
-            warp_lambda::run(warp_service)
-                .await
-                .expect("An error occured");
-            return;
-        }
-    }
+    // let routes = controller::build_routes();
+    // warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 
-    let routes = controller::build_routes();
-    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
+    let routes = warp::any().map(|| "Hello, World!");
+    let warp_service = warp::service(routes);
+    warp_lambda::run(warp_service)
+        .await
+        .expect("An error occured");
 }
