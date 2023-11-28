fe51cc90c1328e9ba8cdf0bb329be147fa411e17
Mircea Gavriliu <gavriliumircea@yahoo.ro>
2023-11-28 13:07:44 +0200

    feat: add unit tests for services and utils

--- src/services/ipfs.rs
+++ src/services/ipfs.rs
@@ -28,10 +28,11 @@ pub async fn upload_to_ipfs(data: PersistentCampaignDto) -> Result<String, reqwe
     dotenv().ok();
     let pinata_api_key = std::env::var("PINATA_API_KEY").expect("PINATA_API_KEY must be set");
     let pinata_secret_api_key = std::env::var("PINATA_SECRET_API_KEY").expect("PINATA_SECRET_API_KEY must be set");
+    let pinata_api_server = std::env::var("PINATA_API_SERVER").expect("PINATA_API_SERVER must be set");
 
     let client = reqwest::Client::new();
 
-    let api_endpoint = "https://api.pinata.cloud/pinning/pinFileToIPFS";
+    let api_endpoint = format!("{}/pinning/pinFileToIPFS", pinata_api_server);
 
     let serialized_data = json!(&data);
     let bytes = serde_json::to_vec(&serialized_data).unwrap();
@@ -59,4 +60,145 @@ pub async fn download_from_ipfs<T: DeserializeOwned>(cid: &str) -> Result<T, req
     let response = reqwest::get(&ipfs_url).await?;
     let data: T = response.json().await?;
     Ok(data)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn try_deserialize_pinata_response_success() {
+        let result: Result<PinataSuccess, serde_json::Error> = try_deserialize_pinata_response(
+            r#"{"IpfsHash": "test_hash", "PinSize": 123, "Timestamp": "2023-04-05T00:00:00Z"}"#,
+        );
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn try_deserialize_pinata_response_fail() {
+        let result: Result<PinataSuccess, serde_json::Error> = try_deserialize_pinata_response("Error message");
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_upload_to_ipfs_ok() {
+        let mut server = mockito::Server::new();
+
+        let host = server.host_with_port();
+        let parts: Vec<&str> = host.split(':').collect();
+        let port = parts[1];
+        let server_host = format!("http://localhost:{}", port);
+
+        // Set environment variables
+        std::env::set_var("PINATA_API_KEY", "mock_pinata_api_key");
+        std::env::set_var("PINATA_SECRET_API_KEY", "mock_pinata_secret_key");
+        std::env::set_var("PINATA_API_SERVER", server_host);
+
+        // Set up mock server
+        let mock = server
+            .mock("POST", "/pinning/pinFileToIPFS")
+            .with_status(200)
+            .with_body(r#"{"IpfsHash": "test_hash", "PinSize": 123, "Timestamp": "2021-01-01T00:00:00Z"}"#)
+            .create();
+
+        // Call the function with a test data object
+        let data = PersistentCampaignDto {
+            total_amount: "128".to_string(),
+            number_of_recipients: 4,
+            root: "test_root".to_string(),
+            merkle_tree: "test_merkle".to_string(),
+            recipients: Vec::new(),
+        };
+        let result = upload_to_ipfs(data).await;
+
+        assert!(result.is_ok());
+        mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_upload_to_ipfs_error() {
+        let mut server = mockito::Server::new();
+
+        let host = server.host_with_port();
+        let parts: Vec<&str> = host.split(':').collect();
+        let port = parts[1];
+        let server_host = format!("http://localhost:{}", port);
+
+        // Set environment variables
+        std::env::set_var("PINATA_API_KEY", "mock_pinata_api_key");
+        std::env::set_var("PINATA_SECRET_API_KEY", "mock_pinata_secret_key");
+        std::env::set_var("PINATA_API_SERVER", server_host);
+
+        // Set up mock server
+        let mock = server
+            .mock("POST", "/pinning/pinFileToIPFS")
+            .with_status(500)
+            .with_body(r#"{"code": "500", "message": "Internal server error"}"#)
+            .create();
+
+        // Call the function with a test data object
+        let data = PersistentCampaignDto {
+            total_amount: "128".to_string(),
+            number_of_recipients: 4,
+            root: "test_root".to_string(),
+            merkle_tree: "test_merkle".to_string(),
+            recipients: Vec::new(),
+        };
+        let result = upload_to_ipfs(data).await;
+
+        let result = result.unwrap();
+        let deserialized_response = try_deserialize_pinata_response(&result);
+        assert!(deserialized_response.is_err());
+        mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_download_from_ipfs_success() {
+        let mut server = mockito::Server::new_with_port(8000);
+
+        let host = server.host_with_port();
+        let parts: Vec<&str> = host.split(':').collect();
+        let port = parts[1];
+        let server_host = format!("http://localhost:{}/", port);
+
+        // Set environment variables
+        std::env::set_var("PINATA_ACCESS_TOKEN", "mock_pinata_access_token");
+        std::env::set_var("IPFS_GATEWAY", server_host);
+
+        // Set up mock server
+        let mock = server
+            .mock("GET", "/valid_cid?pinataGatewayToken=mock_pinata_access_token")
+            .with_status(200)
+            .with_body(r#"{"IpfsHash": "test_hash", "PinSize": 123, "Timestamp": "2021-01-01T00:00:00Z"}"#)
+            .create();
+
+        let result: Result<PinataSuccess, _> = download_from_ipfs("valid_cid").await;
+        assert!(result.is_ok());
+        mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_download_from_ipfs_error() {
+        let mut server = mockito::Server::new_with_port(8000);
+
+        let host = server.host_with_port();
+        let parts: Vec<&str> = host.split(':').collect();
+        let port = parts[1];
+        let server_host = format!("http://localhost:{}/", port);
+
+        // Set environment variables
+        std::env::set_var("PINATA_ACCESS_TOKEN", "mock_pinata_access_token");
+        std::env::set_var("IPFS_GATEWAY", server_host);
+
+        // Set up mock server
+        let mock = server
+            .mock("GET", "/valid_cid?pinataGatewayToken=mock_pinata_access_token")
+            .with_status(500)
+            .with_body(r#"{"code": "500", "message": "Internal server error"}"#)
+            .create();
+
+        let result: Result<PinataSuccess, _> = download_from_ipfs("valid_cid").await;
+        assert!(result.is_err());
+        mock.assert();
+    }
 }
--- src/utils/csv_validator.rs
+++ src/utils/csv_validator.rs
@@ -105,3 +105,135 @@ pub fn validate_csv_header(header: &StringRecord, validators: &[&dyn ColumnValid
     }
     None
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const VALID_ETH_ADDRESS: &str = "0xf31b00e025584486f7c37Cf0AE0073c97c12c634";
+    const INVALID_ETH_ADDRESS: &str = "0xthisIsNotAnAddress";
+    const ADDRESS_VALIDATOR: AddressColumnValidator = AddressColumnValidator;
+
+    const AMOUNT_PATTERN: &str = r"^[+]?\d*\.?\d{0,3}$";
+    fn get_amount_validator() -> AmountColumnValidator {
+        let amount_regex = Regex::new(AMOUNT_PATTERN).unwrap();
+        AmountColumnValidator { regex: amount_regex }
+    }
+
+    #[test]
+    fn is_valid_eth_address_valid() {
+        let result = is_valid_eth_address(VALID_ETH_ADDRESS);
+        assert!(result);
+    }
+
+    #[test]
+    fn is_valid_eth_address_invalid() {
+        let result = is_valid_eth_address(INVALID_ETH_ADDRESS);
+        assert!(!result);
+    }
+
+    #[test]
+    fn address_column_validator_cel_valid() {
+        assert!(ADDRESS_VALIDATOR.validate_cel(VALID_ETH_ADDRESS, 0).is_none());
+    }
+
+    #[test]
+    fn address_column_validator_cel_invalid() {
+        assert!(ADDRESS_VALIDATOR.validate_cel(INVALID_ETH_ADDRESS, 0).is_some());
+    }
+
+    #[test]
+    fn address_column_validator_header_valid() {
+        assert!(ADDRESS_VALIDATOR.validate_header("address").is_none());
+    }
+
+    #[test]
+    fn address_column_validator_header_invalid() {
+        assert!(ADDRESS_VALIDATOR.validate_header("amount").is_some());
+    }
+
+    #[test]
+    fn amount_column_validator_cel_valid() {
+        let validator = get_amount_validator();
+        assert!(validator.validate_cel("123.45", 0).is_none());
+    }
+
+    #[test]
+    fn amount_column_validator_cel_not_a_number() {
+        let validator = get_amount_validator();
+        assert!(validator.validate_cel("thisIsNotANumber", 0).is_some());
+    }
+
+    #[test]
+    fn amount_column_validator_cel_zero_() {
+        let validator = get_amount_validator();
+        assert!(validator.validate_cel("0.0", 0).is_some());
+    }
+
+    #[test]
+    fn amount_column_validator_header_valid() {
+        let validator = get_amount_validator();
+        assert!(validator.validate_header("amount").is_none());
+    }
+
+    #[test]
+    fn amount_column_validator_header_invalid() {
+        let validator = get_amount_validator();
+        assert!(validator.validate_header("address").is_some());
+    }
+
+    #[test]
+    fn validate_csv_row_valid() {
+        let row = StringRecord::from(vec![VALID_ETH_ADDRESS, "489.312"]);
+        let amount_validator = get_amount_validator();
+        let validators: Vec<&dyn ColumnValidator> = vec![&AddressColumnValidator, &amount_validator];
+        assert!(validate_csv_row(&row, 0, &validators).is_empty());
+    }
+
+    #[test]
+    fn validate_csv_row_insufficient_columns() {
+        let row = StringRecord::from(vec![VALID_ETH_ADDRESS]);
+        let amount_validator = get_amount_validator();
+        let validators: Vec<&dyn ColumnValidator> = vec![&AddressColumnValidator, &amount_validator];
+        assert!(!validate_csv_row(&row, 0, &validators).is_empty());
+    }
+
+    #[test]
+    fn validate_csv_row_invalid_address() {
+        let row = StringRecord::from(vec!["thisIsNotAnAddress", "12534"]);
+        let amount_validator = get_amount_validator();
+        let validators: Vec<&dyn ColumnValidator> = vec![&AddressColumnValidator, &amount_validator];
+        assert!(!validate_csv_row(&row, 0, &validators).is_empty());
+    }
+    #[test]
+    fn validate_csv_row_invalid_amount() {
+        let row = StringRecord::from(vec![VALID_ETH_ADDRESS, "12.576757"]);
+        let amount_validator = get_amount_validator();
+        let validators: Vec<&dyn ColumnValidator> = vec![&AddressColumnValidator, &amount_validator];
+        assert!(!validate_csv_row(&row, 0, &validators).is_empty());
+    }
+
+    #[test]
+    fn validate_csv_header_valid() {
+        let header = StringRecord::from(vec!["address", "amount"]);
+        let amount_validator = get_amount_validator();
+        let validators: Vec<&dyn ColumnValidator> = vec![&AddressColumnValidator, &amount_validator];
+        assert!(validate_csv_header(&header, &validators).is_none());
+    }
+
+    #[test]
+    fn validate_csv_header_invalid_address_header() {
+        let header = StringRecord::from(vec!["address_invalid", "amount"]);
+        let amount_validator = get_amount_validator();
+        let validators: Vec<&dyn ColumnValidator> = vec![&AddressColumnValidator, &amount_validator];
+        assert!(validate_csv_header(&header, &validators).is_some());
+    }
+
+    #[test]
+    fn validate_csv_header_invalid_amount_header() {
+        let header = StringRecord::from(vec!["address", "amount_invalid"]);
+        let amount_validator = get_amount_validator();
+        let validators: Vec<&dyn ColumnValidator> = vec![&AddressColumnValidator, &amount_validator];
+        assert!(validate_csv_header(&header, &validators).is_some());
+    }
+}
